8e19ad151f78ebb7b33e8a9f7db93faf77352ea3
Javier Castañeda <javier.castaneda@gaiadesign.com.mx>
2019-07-09 20:40:43 -0500

    Solved

--- FirstPlayground.playground/Contents.swift
+++ FirstPlayground.playground/Contents.swift
@@ -1,48 +1,53 @@
 import UIKit
-// Comentario
 
-//Declarando constantes
-let zeroAsInt: Int = 0
-let zeroAsDouble: Double = 0.0
-let zeroAsString: String = "0"
+struct StructPerson {
+    let name: String
+    let lastname: String
+    let birthdate: String
+    var age: Int {
+        //Create formatter
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let birthDateAsDate = dateFormatter.date(from: birthdate)
+        guard let birthDate = birthDateAsDate else {
+            return 0
+        }
+        let calendar = Calendar(identifier: .gregorian)
+        let now = Date()
+        let components = calendar.dateComponents([.year], from: birthDate, to: now)
 
-//Imprimiendo en consola
-print(zeroAsInt)
-print(zeroAsDouble)
-print(zeroAsString)
+        return components.year ?? 0
+    }
 
-// Variables
-var integerNumber: Int = 1
-var doubleNumber: Double = 1
-var stringNumber: String = "1"
-
-let boolean: Bool = true
-var anotherBoolean: Bool = false
-
-//Funciones
-func sum(first: Int, second: Int) -> Int {
-    return first + second
 }
 
-
-// Estructuras
-
-struct Person {
-    var name: String
+class ClassPerson {
+    let name: String
+    let lastname: String
+    let birthdate: String
     var age: Int
-    var hobbie: String
 
-    func selfIntroduce() {
-        print("My name is " + name)
-        print("I'm \(age) years old")
-        print("My hobbie is " + hobbie)
+    init(name: String, lastname: String, birthdate: String, age: Int) {
+        self.name = name
+        self.lastname = lastname
+        self.birthdate = birthdate
+        self.age = age
     }
 }
 
-let firstPerson = Person(name: "Hugo", age: 21, hobbie: "Jugar cartas")
-let secondPerson = Person(name: "Paco", age: 21, hobbie: "Videojuegos")
-let thirdPerson = Person(name: "Luis", age: 21, hobbie: "Hacer deporte")
+let classPerson =  ClassPerson(name: "Javier", lastname: "Castañeda", birthdate: "1987-09-23", age: 31)
 
-firstPerson.selfIntroduce()
-secondPerson.selfIntroduce()
-thirdPerson.selfIntroduce()
+let structPerson = StructPerson(name: "Javier", lastname: "Castañeda", birthdate: "1987-05-23")
+
+
+let secondClassPerson = classPerson
+secondClassPerson.age = 32
+
+print(classPerson.age)
+print(secondClassPerson.age)
+
+var secondStructPerson = structPerson
+
+print("--------Structs----------")
+print(structPerson.age)
+print(secondStructPerson.age)
